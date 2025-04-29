c27e6877aadd0abb8d412b5d8369f2f75517e48f
yadav767 <yyadavaayush767@gmail.com>
2025-04-29 22:49:37 +0530

    happy number

---
+++ Day14.js
@@ -0,0 +1,17 @@
+let n = 19;
+function happyNumber(n) {
+    let set = new Set();
+    while (true) {
+        let sum = 0;
+        while (n > 0) {
+            let rem = n % 10;
+            sum += (rem * rem);
+            n = Math.floor(n / 10);
+        }
+        if (sum == 1) return true;
+        else if (set.has(sum)) return false;
+        else set.add(sum);
+        n = sum;
+    }
+}
+console.log(happyNumber(n));

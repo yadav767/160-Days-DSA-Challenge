dd86690c671eed144ed3da697d9bc5dff0c5cbdc
yadav767 <yyadavaayush767@gmail.com>
2025-06-14 03:04:27 +0530

    Removes duplicates from linked list

---
+++ Day64.js
@@ -0,0 +1,13 @@
+var deleteDuplicates = function (head) {
+
+    let temp = head;
+    while (temp != null && temp.next != null) {
+        if (temp.val === temp.next.val) {
+            temp.next = temp.next.next;
+        }
+        else {
+            temp = temp.next;
+        }
+    }
+    return head;
+}

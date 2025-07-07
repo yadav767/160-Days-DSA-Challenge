7b68e9b2de7a949fe53688c5ed16d39e25fcb844
yadav767 <yyadavaayush767@gmail.com>
2025-07-07 23:16:03 +0530

    sort color

---
+++ Day81.js
@@ -0,0 +1,18 @@
+var sortColors = function (nums) {
+    let low = 0, mid = 0, high = nums.length - 1;
+
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            [nums[low], nums[mid]] = [nums[mid], nums[low]];
+            low++;
+            mid++;
+        } else if (nums[mid] === 1) {
+            mid++;
+        } else {
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
+        }
+    }
+    return nums
+
+}

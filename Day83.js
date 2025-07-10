b7e73402eeda1433ad387c38b3d4f7c185afef70
yadav767 <yyadavaayush767@gmail.com>
2025-07-10 22:29:18 +0530

    inorder traversal

---
+++ Day83.js
@@ -0,0 +1,19 @@
+
+
+
+var inorderTraversal = function (root) {
+
+    let result = [];
+    function dfs(root) {
+
+        if (root === null) return;
+        dfs(root.left)
+        result.push(root.val);
+        dfs(root.right);
+
+    }
+
+    dfs(root);
+    return result;
+
+}

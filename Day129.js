1ec8b25fe92ee86c85b9ec915a52266abeb58477
yadav767 <yyadavaayush767@gmail.com>
2025-10-03 21:38:54 +0530

    Fizz Buzz

---
+++ Day129.js
@@ -0,0 +1,19 @@
+//Leetcode Problem 412: Fizz Buzz.
+let n = 15;
+var fizzBuzz = function (n) {
+    let result = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 == 0) {
+            result.push("Fizz");
+        } else if (i % 5 == 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i.toString())
+        }
+
+    }
+    return result
+};
+console.log(fizzBuzz(n));

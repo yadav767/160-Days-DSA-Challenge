2451f839ce0a534dcf2497d8bff0756b1c42d7af
yadav767 <yyadavaayush767@gmail.com>
2025-11-13 23:25:12 +0530

    Create Target Array in the Given Order

---
+++ Day151.js
@@ -0,0 +1,12 @@
+//LeetCode Problem 1389: Create Target Array in the Given Order
+let nums = [0, 1, 2, 3, 4];
+let index = [0, 1, 2, 2, 1];
+
+var createTargetArray = function (nums, index) {
+    let target = [];
+    for (let i = 0; i < nums.length; i++) {
+        target.splice(index[i], 0, nums[i]);
+    }
+    return target;
+}
+console.log(createTargetArray(nums, index)); 

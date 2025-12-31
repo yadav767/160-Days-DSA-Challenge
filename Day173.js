9a483ac7ac4116575e304a238045a6c7ea6914ba
yadav767 <yyadavaayush767@gmail.com>
2025-12-31 23:56:27 +0530

    Add Digits

---
+++ Day173.js
@@ -0,0 +1,8 @@
+//Leetcode Problem 258: Add Digits
+let num = 38;
+
+var addDigits = function (num) {
+    if (num === 0) return 0;
+    return num % 9 === 0 ? 9 : num % 9;
+};
+console.log(addDigits(num));
